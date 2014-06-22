6336be258ec6916b741e3715efff7f3f1e8c511a
jkwaldman <jkwaldman1@gmail.com>
2014-06-22 03:22:41 -0400

    cacheMatrix.R updated with solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cacheMatrix creates a cacheable matrix object given a matrix
+# cacheSolve takes that matrix, checks if it has been computed, and
+# returns the inverted matrix.
 
-## Write a short comment describing this function
+# Creates a cacheable matrix object given a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cacheVal <- NULL
+    setMatrix <- function(y) {
+        x <<- y
+        cacheVal <<- NULL
+    }
+    getMatrix <- function() x
+    setCachedMatrix <- function(matrix) cacheVal <<- matrix
+    getCachedMatrix <- function() cacheVal
+    # Creates the matrix object
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setCachedMatrix = setCachedMatrix, getCachedMatrix = 
+         getCachedMatrix)
 }
 
 
-## Write a short comment describing this function
+# Takes the result of makeCacheMatrix() and find the inversion
+# of that matrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cacheVal <- x$getCachedMatrix()
+    #Only true if the value has been computed before
+    if(!is.null(cacheVal)){
+        message("Getting cached data..")
+        return(cacheVal)
+    }
+    #Hasn't been computed, so find the matrix inversion
+    matrixVal <- x$getMatrix()
+    solvedMatrix <- solve(matrixVal)
+    x$setCachedMatrix(solvedMatrix)
+    solvedMatrix
+
+# Returns a matrix that is the inverse of the given matrix
 }
